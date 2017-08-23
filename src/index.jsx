7d7e30f357324be2359e1e3660c8c89fdc9200fb
Jerry Krusinski <jkruzr@gmail.com>
2017-08-23 14:29:07 -0500

    renders redux devtools based on environment

--- src/index.jsx
+++ src/index.jsx
@@ -7,14 +7,19 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
 import * as reducers from './reducers';
+import { env } from '../config';
 
 import App from './components/App';
 
 /* eslint-disable no-underscore-dangle */
-const reducer = combineReducers(reducers);
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  && env === 'dev'
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  : compose;
 /* eslint-enable */
+
+const reducer = combineReducers(reducers);
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 render(
   <Provider store={store}>
