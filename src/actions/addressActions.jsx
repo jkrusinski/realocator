29d01ad4d702bc18051a91f9e97c58317bd03565
Jerry Krusinski <jkruzr@gmail.com>
2017-08-23 14:30:56 -0500

    adds query to address action creators

--- src/actions/addressActions.jsx
+++ src/actions/addressActions.jsx
@@ -1,19 +1,59 @@
+import axios from 'axios';
 import {
-  UPDATE_FIRST_ADDRESS,
-  UPDATE_SECOND_ADDRESS,
-  CLEAR_ADDRESSES,
+  UPDATE_ADDRESS_QUERY,
+  UPDATE_ADDRESS_SUGGESTIONS,
+  CLEAR_ADDRESS,
 } from '../constants';
 
-export const updateFirstAddress = address => ({
-  type: UPDATE_FIRST_ADDRESS,
-  payload: address,
+export const queryGooglePlaces = search => axios({
+  method: 'get',
+  url: 'http://localhost:3000/api/address',
+  params: { search },
+}).then(res => res.data.results);
+
+const updateAddressQuery = (first, second) => ({
+  type: UPDATE_ADDRESS_QUERY,
+  first,
+  second,
 });
 
-export const updateSecondAddress = address => ({
-  type: UPDATE_SECOND_ADDRESS,
-  payload: address,
+const updateAddressSuggestions = (first, second) => ({
+  type: UPDATE_ADDRESS_SUGGESTIONS,
+  first,
+  second,
 });
 
-export const clearAddresses = () => ({
-  type: CLEAR_ADDRESSES,
+export const clearAddress = (first, second) => ({
+  type: CLEAR_ADDRESS,
+  first,
+  second,
+});
+
+export const queryAddress = (first, second) => ((dispatch) => {
+  if (first === '') {
+    dispatch(clearAddress(true, false));
+  }
+
+  if (second === '') {
+    dispatch(clearAddress(false, true));
+  }
+
+  // will update query for empty string (falsy)
+  dispatch(updateAddressQuery(first, second));
+
+  if (first) {
+    queryGooglePlaces(first)
+      .then((results) => {
+        dispatch(updateAddressSuggestions(results, null));
+      })
+      .catch(console.error);
+  }
+
+  if (second) {
+    queryGooglePlaces(second)
+      .then((results) => {
+        dispatch(updateAddressSuggestions(null, results));
+      })
+      .catch(console.error);
+  }
 });
