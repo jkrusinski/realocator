514e6aeed06123aad87e061b389118d714bcdaab
Jerry Krusinski <jkruzr@gmail.com>
2017-08-23 14:30:09 -0500

    creates api route

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+const { GOOGLE_API_KEY } = require('./config');
 
 const app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'src/public')));
+
+app.get('/api/address', (req, res, next) => {
+  const query = req.query.search;
+  const options = {
+    method: 'get',
+    url: 'https://maps.googleapis.com/maps/api/place/textsearch/json',
+    params: {
+      key: GOOGLE_API_KEY,
+      location: '30.2672,-97.7431',
+      radius: '50000',
+      query,
+    },
+  };
+
+  axios(options)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/index.html'));
