f8b77289870f74ce1bf08889e21d599d8ced391c
Jerry Krusinski <jkruzr@gmail.com>
2017-08-22 22:24:13 -0500

    connects redux to react

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,24 @@
-/* global document */
+/* global document, window */
 
 import React from 'react';
 import { render } from 'react-dom';
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import * as reducers from './reducers';
 
 import App from './components/App';
 
-render(<App />, document.getElementById('app'));
+/* eslint-disable no-underscore-dangle */
+const reducer = combineReducers(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+/* eslint-enable */
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app'),
+);
