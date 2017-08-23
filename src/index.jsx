ec3a1cbbf53e01b6d042721e3a6aa81a1cac1d52
Jerry Krusinski <jkruzr@gmail.com>
2017-08-22 20:35:20 -0500

    renders to page

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,6 @@
-console.log('hello world');
+/* global document */
+
+import React from 'react';
+import { render } from 'react-dom';
+
+render(<div>Some crazy text</div>, document.getElementById('app'));
