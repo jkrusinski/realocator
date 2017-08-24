749f943440c288dba0d105ac11a94f32a47a8778
Jerry Krusinski <jkruzr@gmail.com>
2017-08-24 13:59:20 -0500

    styles buttons

--- src/components/Addresses.jsx
+++ src/components/Addresses.jsx
@@ -9,9 +9,21 @@ const styles = {
     display: 'flex',
     justifyContent: 'space-evenly',
   },
-  clearSelection: {
+  actions: {
     width: '100%',
     textAlign: 'center',
+  },
+  button: {
+    margin: '20px 10px',
+    padding: '20px',
+    backgroundColor: 'lime',
+    fontFamily: 'inherit',
+    fontSize: '16px',
+    border: 'none',
+    borderRadius: '5px',
+    '&:hover': {
+      cursor: 'pointer',
+    },
   },
 };
 
@@ -35,9 +47,9 @@ const Addresses = ({ addresses: { first, second }, clearAddresses, inputs, searc
         suggestions={suggestions[1]}
       />
     </div>
-    <div className={classes.clearSelection}>
-      <button onClick={clearAddresses}>Clear Selection</button>
-      <button onClick={fetchResults}>Search Realtors</button>
+    <div className={classes.actions}>
+      <button className={classes.button} onClick={clearAddresses}>Clear Selection</button>
+      <button className={classes.button} onClick={fetchResults}>Search Realtors</button>
     </div>
   </div>
 );
